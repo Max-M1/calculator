4f0fc8a963cb4954956a2561a2868c721bc2e248
Max <mmaslov848@gmail.com>
2025-02-16 14:39:38 +0200

    реалізовано інтерфейс проекту додатку 'Калькулятор'

--- calculator.py
+++ calculator.py
@@ -6,7 +6,6 @@ class CalculatorApp:
         self.root = root
         self.root.title("Калькулятор")
         self.root.geometry("300x400")
-
         self.create_widgets()
 
     def create_widgets(self):
@@ -14,7 +13,40 @@ class CalculatorApp:
         self.entry.pack(fill="both", padx=10, pady=10)
 
         self.buttons_frame = tk.Frame(self.root)
-        self.buttons_frame.pack()
+        self.buttons_frame.pack(expand=True, fill="both")
+
+        btn_clear = tk.Button(
+            self.buttons_frame, text="C", font=("Arial", 18), command=self.clear_entry
+        )
+        btn_clear.grid(row=0, column=0, columnspan=4, sticky="nsew")
+
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            ["0", ".", "=", "+"],
+        ]
+
+        for i, row in enumerate(buttons):
+            for j, btn_text in enumerate(row):
+                if btn_text == "=":
+                    btn_command = self.calculate
+                else:
+                    btn_command = lambda value=btn_text: self.add_to_expression(value)
+                btn = tk.Button(
+                    self.buttons_frame,
+                    text=btn_text,
+                    font=("Arial", 18),
+                    command=btn_command,
+                )
+                btn.grid(row=i + 1, column=j, sticky="nsew")
+
+        total_rows = len(buttons) + 1
+        total_columns = 4
+        for i in range(total_rows):
+            self.buttons_frame.rowconfigure(i, weight=1)
+        for j in range(total_columns):
+            self.buttons_frame.columnconfigure(j, weight=1)
 
 
 if __name__ == "__main__":
